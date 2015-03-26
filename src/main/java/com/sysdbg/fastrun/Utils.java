72086adb80607129a7ee218ae3fe23531b0aabf6
Xin Zhao <xinzhao@amazon.com>
2015-03-26 09:48:42 +0800

    finish InputStreamToString and InputStreamToByteArray

--- src/main/java/com/sysdbg/fastrun/Utils.java
+++ src/main/java/com/sysdbg/fastrun/Utils.java
@@ -12,6 +12,12 @@ public class Utils {
     private static final byte[] EMPTY_BYTE_ARRAY = {};
     private static final int BUFFER_SIZE = 128;
 
+    /**
+     * Read whole input out into a byte array.
+     * @param input The input stream.
+     * @return The byte array read from input. If input is null, return a zero-length array.
+     * @throws IOException Some error happen when read input.
+     */
     public static byte[] InputStreamToByteArray(InputStream input) throws IOException {
         if (input == null)
             return EMPTY_BYTE_ARRAY;
@@ -33,7 +39,28 @@ public class Utils {
         // get final byte array
         return output.toByteArray();
     }
-    public static String InputStreamToString(InputStream input, Charset charset) throws IOException {
-        return null;
+
+    /**
+     * Read whole input, and translate the bytes into string.
+     * @param input The input stream to read. This parameter can be null, which means a zero-length stream.
+     * @param charset The charset for the stream bytes. This parameter cannot be null.
+     *                The invalid character would be translated into default char of charset.
+     * @return The result string.
+     * @throws IOException Some error happen when reading input.
+     * @throws NullPointerException The charset is null.
+     */
+    public static String InputStreamToString(InputStream input, Charset charset)
+            throws IOException, NullPointerException {
+        if (charset == null) {
+            throw new NullPointerException("charset is null");
+        }
+
+        byte[] data = InputStreamToByteArray(input);
+        if (data.length == 0) {
+            return "";
+        }
+
+        String result = new String(data, charset);
+        return result;
     }
 }
--- src/test/java/com/sysdbg/fastrun/UtilsTest.java
+++ src/test/java/com/sysdbg/fastrun/UtilsTest.java
@@ -1,10 +1,12 @@
 package com.sysdbg.fastrun;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -13,6 +15,38 @@ import static org.mockito.Mockito.*;
  * Created by crady on 3/25/2015.
  */
 public class UtilsTest {
+    private static final String  UTF8 = "UTF-8";
+    private static final String  HELLO_WORLD = "HelloWorld";
+    private static final String  EMPTY_STRING = "";
+    private static final Charset UTF8_CHARSET;
+
+    private byte[] mEmptyBytes;
+    private ByteArrayInputStream mEmptyInputStream;
+
+    private byte[] mHelloWorldBytes;
+    private ByteArrayInputStream mHelloWorldInputStream;
+
+    private InputStream mExceptionStream;
+
+    static {
+        UTF8_CHARSET = Charset.forName(UTF8);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        mEmptyBytes = new byte[0];
+        mEmptyInputStream = new ByteArrayInputStream(mEmptyBytes);
+
+        mHelloWorldBytes = HELLO_WORLD.getBytes(UTF8_CHARSET);
+        mHelloWorldInputStream = new ByteArrayInputStream(mHelloWorldBytes);
+
+        mExceptionStream = mock(InputStream.class);
+        IOException exception = new IOException();
+        when(mExceptionStream.read()).thenThrow(exception);
+        when(mExceptionStream.read(any(byte[].class))).thenThrow(exception);
+        when(mExceptionStream.read(any(byte[].class), anyInt(), anyInt())).thenThrow(exception);
+    }
+
     @Test
     public void emptyInputStreamToArray() throws Exception {
         byte[] result = Utils.InputStreamToByteArray(null);
@@ -23,18 +57,46 @@ public class UtilsTest {
 
     @Test
     public void normalStreamToArray() throws Exception {
-        byte[] input_array = {0x1, 0x2, 0x3, 0x4, 0x5};
-        ByteArrayInputStream in = new ByteArrayInputStream(input_array);
+        byte[] result = Utils.InputStreamToByteArray(mHelloWorldInputStream);
+        assertArrayEquals(mHelloWorldBytes, result);
+    }
 
-        byte[] result = Utils.InputStreamToByteArray(in);
-        assertArrayEquals(input_array, result);
+    @Test
+    public void emptyStreamToArray() throws Exception {
+        byte[] result = Utils.InputStreamToByteArray(mEmptyInputStream);
+        assertArrayEquals(mEmptyBytes, result);
     }
 
     @Test(expected = IOException.class)
     public void exceptionStreamToArray() throws Exception {
-        InputStream is = mock(InputStream.class);
-        IOException exception = new IOException();
-        when(is.read()).thenThrow(exception);
-        when(is.read(anyObject())).thenThrow(exception);
+        Utils.InputStreamToByteArray(mExceptionStream);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void nullCharsetStreamToString() throws Exception {
+        Utils.InputStreamToString(mHelloWorldInputStream, null);
+    }
+
+    @Test
+    public void nullInputStreamToString() throws Exception {
+        String result = Utils.InputStreamToString(null, UTF8_CHARSET);
+        assertEquals(EMPTY_STRING, result);
+    }
+
+    @Test
+    public void emptyInputStreamToString() throws Exception {
+        String result = Utils.InputStreamToString(mEmptyInputStream, UTF8_CHARSET);
+        assertEquals(EMPTY_STRING, result);
+    }
+
+    @Test
+    public void normalInputStreamToString() throws Exception {
+        String result = Utils.InputStreamToString(mHelloWorldInputStream, UTF8_CHARSET);
+        assertEquals(HELLO_WORLD, result);
+    }
+
+    @Test(expected = IOException.class)
+    public void exceptionInputStreamToString() throws Exception {
+        Utils.InputStreamToString(mExceptionStream, UTF8_CHARSET);
     }
 }
